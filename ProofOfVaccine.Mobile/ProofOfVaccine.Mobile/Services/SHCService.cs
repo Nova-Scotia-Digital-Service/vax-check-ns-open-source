f85cea043807a4a4ad772954f3e035d6631a19a5
AjaySivanesu <ajay@sivanesu.com>
2021-09-15 13:09:15 -0400

    Create SHCService.cs

---
+++ ProofOfVaccine.Mobile/ProofOfVaccine.Mobile/Services/SHCService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProofOfVaccine.Mobile.Services
+{
+    public interface ISHCService
+    {
+
+    }
+
+    public class SHCService : ISHCService
+    {
+
+    }
+}
