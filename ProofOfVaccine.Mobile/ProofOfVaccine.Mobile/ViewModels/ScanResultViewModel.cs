8fb6c90ae45b8a842ea4fc5f8e49ab9f1b6bdb00
AjaySivanesu <ajay@sivanesu.com>
2021-09-16 08:26:29 -0400

    Basic Navigation Flow

--- ProofOfVaccine.Mobile/ProofOfVaccine.Mobile/ViewModels/ScanResultViewModel.cs
+++ ProofOfVaccine.Mobile/ProofOfVaccine.Mobile/ViewModels/ScanResultViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ProofOfVaccine.Mobile.ViewModels
 {
     public class ScanResultViewModel : BaseViewModel
     {
+        public Command NewScanCommand { get; set; }
+
+        public ScanResultViewModel()
+        {
+            NewScanCommand = new Command(async () => await Shell.Current.GoToAsync("../ScanPage", true));
+        }
     }
 }
