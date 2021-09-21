997c57bfd50a5c17062acf37847915dcb4ac8615
AjaySivanesu <ajay@sivanesu.com>
2021-09-21 10:17:45 -0400

    App Center SDK Integration

--- ProofOfVaccine.Mobile/ProofOfVaccine.Mobile.Android/MainActivity.cs
+++ ProofOfVaccine.Mobile/ProofOfVaccine.Mobile.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ProofOfVaccine.Mobile.Droid
 {
-    [Activity(Label = "ProofOfVaccine.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, 
+    [Activity(Label = "Proof Of Vaccine", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, 
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, 
         ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
--- ProofOfVaccine.Mobile/ProofOfVaccine.Mobile/App.xaml.cs
+++ ProofOfVaccine.Mobile/ProofOfVaccine.Mobile/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using ProofOfVaccine.Mobile.Services;
-using ProofOfVaccine.Mobile.Views;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+
+using ProofOfVaccine.Mobile.Services;
+
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -23,6 +28,7 @@ namespace ProofOfVaccine.Mobile
 
         protected override void OnStart()
         {
+            InitializeAppCenter();
         }
 
         protected override void OnSleep()
@@ -31,6 +37,13 @@ namespace ProofOfVaccine.Mobile
 
         protected override void OnResume()
         {
+        }
+
+        private void InitializeAppCenter()
+        {
+            AppCenter.Start("android=c8da9da7-50dd-42a8-b7f4-312370215d5a;" +
+                            "ios=b680b5fc-214c-49a1-9bce-5decfe8b445a;",
+                            typeof(Analytics), typeof(Crashes), typeof(Distribute));
         }
     }
 }
