01939a01712e574bbb85155eb50aa1d3dd21e59b
Andrew.Kenny <andrewkenny@T4Gs-MacBook-Pro-3.local>
2021-09-27 12:40:08 -0300

    Removed comments on the FadeAnimation

--- ProofOfVaccine.Mobile/ProofOfVaccine.Mobile/Animations/FadeAnimation.cs
+++ ProofOfVaccine.Mobile/ProofOfVaccine.Mobile/Animations/FadeAnimation.cs
@@ -3,41 +3,17 @@ using Xamarin.Forms;
 
 namespace ProofOfVaccine.Mobile.Animations
 {
-    /// <summary>
-    /// An animation that can be used to change the opacity of a view from one opacity level to another.
-    /// </summary>
     public class FadeAnimation : IAnimation<double>
     {
         private readonly string _name = nameof(FadeAnimation);
         private double _opacity;
 
-        /// <summary>
-        /// The readonly opacity that the view starts with.
-        /// </summary>
         public double FromOpacity { get; }
-
-        /// <summary>
-        /// The readonly opacity that the view ends with.
-        /// </summary>
         public double ToOpacity { get; }
-
-        /// <summary>
-        /// The readonly duration of the animation.
-        /// </summary>
         public TimeSpan Duration { get; }
 
-        /// <summary>
-        /// The readonly easing function the animation uses as part of the transformation.
-        /// </summary>
         public Easing Easing { get; }
 
-        /// <summary>
-        /// Constructor used to create the animation.
-        /// </summary>
-        /// <param name="fromOpacity">Used to set <seealso cref="FromOpacity"/> of the animation. </param>
-        /// <param name="toOpacity">Used to set <seealso cref="ToOpacity"/> of the animation.</param>
-        /// <param name="duration">Used to set <seealso cref="Duration"/> of the animation.</param>
-        /// <param name="easing">Used to set <seealso cref="Easing"/> of the animation.</param>
         public FadeAnimation(double fromOpacity,
             double toOpacity,
             TimeSpan duration,
@@ -51,12 +27,6 @@ namespace ProofOfVaccine.Mobile.Animations
             _opacity = fromOpacity;
         }
 
-        /// <summary>
-        /// The method that triggers the animation start. When called the Action passed in will be used to change from the <see cref="FromOpacity"/> to the <seealso cref="ToOpacity"/> over the defined <seealso cref="Duration"/> using the <seealso cref="Easing"/> function.
-        /// This can be cancelled using the <seealso cref="AnimationExtensions.AbortAnimation(IAnimatable, string)"/> with the owner being the <seealso cref="Application.Current.MainPage"/> using the handle of the animation class.
-        /// </summary>
-        /// <param name="onValueCallback">This Action is a callback used to modify the Opacity property of a view. The Opacity passed back is transformed from <seealso cref="FromOpacity"/> to <seealso cref="ToOpacity"/> over the <seealso cref="Duration"/> using the <seealso cref="Easing"/> function.</param>
-        /// <param name="onComplete">Called when the animation is completed.</param>
         public void Start(Action<double> onValueCallback, Action onComplete = null)
         {
 
