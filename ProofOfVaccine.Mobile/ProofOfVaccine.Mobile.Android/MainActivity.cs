6139576b022b75c44ab16350197491d8c3a29684
Andrew.Kenny <andrewkenny@T4Gs-MacBook-Pro-3.local>
2021-10-05 23:50:36 -0300

    Added a couple custom renderers to fix visual issues with the header. Another for iOS button image spacing. Fixed up missing icons and fonts. Finally, fixed up the home page to work with all screen sizes. Verified from ipod to ipad. Used FlexLayout and seems to be working well.

--- ProofOfVaccine.Mobile/ProofOfVaccine.Mobile.Android/MainActivity.cs
+++ ProofOfVaccine.Mobile/ProofOfVaccine.Mobile.Android/MainActivity.cs
@@ -16,6 +16,8 @@ namespace ProofOfVaccine.Mobile.Droid
         {
             base.SetTheme(Resource.Style.MainTheme);
 
+            ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
