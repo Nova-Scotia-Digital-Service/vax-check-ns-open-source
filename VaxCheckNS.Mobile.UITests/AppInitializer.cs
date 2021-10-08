10971441674cc1b913360bf8291ff0e2ec3fef06
AjaySivanesu <ajay@sivanesu.com>
2021-10-08 12:50:03 -0400

    Test Configuration

--- VaxCheckNS.Mobile.UITests/AppInitializer.cs
+++ VaxCheckNS.Mobile.UITests/AppInitializer.cs
@@ -16,6 +16,7 @@ namespace VaxCheckNS.Mobile.UITests
                     .StartApp();
             }
 
+
             return ConfigureApp.iOS.StartApp();
         }
     }
--- VaxCheckNS.Mobile/VaxCheckNS.Mobile/App.xaml.cs
+++ VaxCheckNS.Mobile/VaxCheckNS.Mobile/App.xaml.cs
@@ -1,51 +1,72 @@
 using VaxCheckNS.Mobile.Services;
-﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 namespace VaxCheckNS.Mobile
 {
-    public partial class App : Application
-    {
-        private readonly ISHCService _shcService;
+	public partial class App : Application
+	{
+		private ISHCService _shcService;
 
-        public App()
-        {
-            InitializeComponent();
+		public App()
+		{
+			InitializeComponent();
 
-            DependencyService.RegisterSingleton<IErrorManagementService>(new ErrorManagementService());
-            DependencyService.RegisterSingleton<IDataService>(new DataService());
-            DependencyService.RegisterSingleton<IConnectivityService>(new ConnectivityService());
-            //TODO: integrate into DataService and Remove 
-            DependencyService.RegisterSingleton<ILocalDataService>(new LocalDataService());
+#if ENABLE_TEST_CLOUD
+			RegisterMockServices();
+#else
+			RegisterServices();
+#endif
 
-            _shcService = new SHCService();
-            DependencyService.RegisterSingleton(_shcService);
+			MainPage = new AppShell();
+		}
 
-            MainPage = new AppShell();
-        }
+		private void RegisterServices()
+		{
+			DependencyService.RegisterSingleton<IErrorManagementService>(new ErrorManagementService());
+			DependencyService.RegisterSingleton<IDataService>(new DataService());
+			DependencyService.RegisterSingleton<IConnectivityService>(new ConnectivityService());
+			//TODO: integrate into DataService and Remove 
+			DependencyService.RegisterSingleton<ILocalDataService>(new LocalDataService());
 
-        protected override async void OnStart()
-        {
-            InitializeAppCenter();
-            await _shcService.InitializeAsync();
-        }
+			_shcService = new SHCService();
+			DependencyService.RegisterSingleton(_shcService);
+		}
 
-        protected override void OnSleep()
-        {
-        }
+		private void RegisterMockServices()
+		{//TODO: replace services with mock
+			DependencyService.RegisterSingleton<IErrorManagementService>(new ErrorManagementService());
+			DependencyService.RegisterSingleton<IDataService>(new DataService());
+			DependencyService.RegisterSingleton<IConnectivityService>(new ConnectivityService());
+			//TODO: integrate into DataService and Remove 
+			DependencyService.RegisterSingleton<ILocalDataService>(new LocalDataService());
 
-        protected override async void OnResume()
-        {
-            await _shcService.TryUpdateKeyset();
-        }
+			_shcService = new SHCService();
+			DependencyService.RegisterSingleton(_shcService);
+		}
 
-        private void InitializeAppCenter()
-        {
-            AppCenter.Start("android=c8da9da7-50dd-42a8-b7f4-312370215d5a;" +
-                            "ios=b680b5fc-214c-49a1-9bce-5decfe8b445a;",
-                            typeof(Analytics), typeof(Crashes));
-        }
-    }
+		protected override async void OnStart()
+		{
+			InitializeAppCenter();
+			await _shcService.InitializeAsync();
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override async void OnResume()
+		{
+			await _shcService.TryUpdateKeyset();
+		}
+
+		private void InitializeAppCenter()
+		{
+			AppCenter.Start("android=c8da9da7-50dd-42a8-b7f4-312370215d5a;" +
+							"ios=b680b5fc-214c-49a1-9bce-5decfe8b445a;",
+							typeof(Analytics), typeof(Crashes));
+		}
+	}
 }
