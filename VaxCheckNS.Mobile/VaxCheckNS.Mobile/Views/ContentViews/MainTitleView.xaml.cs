4c55e4e3e73f805dfe1db6e00083a90f231ead2a
Andrew.Kenny <andrewkenny@T4Gs-MacBook-Pro-3.local>
2021-10-16 02:11:48 -0300

    Added a couple tweaks and prepared the FAQ page.

---
+++ VaxCheckNS.Mobile/VaxCheckNS.Mobile/Views/ContentViews/MainTitleView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace VaxCheckNS.Mobile.Views.ContentViews
+{
+    public partial class MainTitleView : ContentView
+    {
+        public MainTitleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
