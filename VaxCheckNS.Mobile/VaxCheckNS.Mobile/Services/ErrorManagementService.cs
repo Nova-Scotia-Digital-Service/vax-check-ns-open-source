4c32ae470a2e27e5cdde59312a4013f92d899ba2
AjaySivanesu <ajay@sivanesu.com>
2021-10-18 07:35:03 -0400

    Bad Scan analytics

--- VaxCheckNS.Mobile/VaxCheckNS.Mobile/Services/ErrorManagementService.cs
+++ VaxCheckNS.Mobile/VaxCheckNS.Mobile/Services/ErrorManagementService.cs
@@ -4,37 +4,57 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using VaxCheckNS.Mobile.DTOs;
+using Xamarin.Essentials;
 
 namespace VaxCheckNS.Mobile.Services
 {
-    public interface IErrorManagementService
-    {
-        void HandleError(Exception ex);
-    }
+	public interface IErrorManagementService
+	{
+		void HandleError(Exception ex);
+		void BadScan(ProofOfVaccinationData data, string message);
+	}
 
-    public class ErrorManagementService : IErrorManagementService
-    {
+	public class ErrorManagementService : IErrorManagementService
+	{
 
-        public ErrorManagementService()
-        {
+		public ErrorManagementService()
+		{
 
-        }
+		}
 
-        public void HandleError(Exception ex)
-        {
-#if !DEBUG
+		public void HandleError(Exception ex)
+		{
+#if RELEASE
             Report(ex);
 #endif
-        }
+		}
 
-        private void Report(Exception ex)
-        {
-            Crashes.TrackError(ex);
-        }
+		private void Report(Exception ex)
+		{
+			Crashes.TrackError(ex);
+		}
 
-        private void InternalReport(Exception ex)
-        {
+		private void InternalReport(Exception ex)
+		{
 
-        }
-    }
+		}
+
+		public void BadScan(ProofOfVaccinationData data, string message)
+		{
+
+			Analytics.TrackEvent("Invalid Scan",
+				  new Dictionary<string, string>
+				  {
+					{ "Time", DateTime.UtcNow.ToString() },
+					{ "Issuer", data.Issuer},
+					{ "ExceptionMessage", message},
+					{ "Version", VersionTracking.CurrentVersion},
+					{ "Platform", DeviceInfo.Platform.ToString()}
+				  });
+
+
+
+		}
+	}
 }
