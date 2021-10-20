06cdd20ede072b4643a90b93e9112cffec13bfdb
Andrew.Kenny <andrewkenny@T4Gs-MacBook-Pro-3.local>
2021-10-20 19:52:19 -0300

    Testing

--- VaxCheckNS.Mobile/VaxCheckNS.Mobile.iOS/appcenter-pre-build.sh
+++ VaxCheckNS.Mobile/VaxCheckNS.Mobile.iOS/appcenter-pre-build.sh
@@ -17,22 +17,11 @@ fi
 
 echo "Looking to update version to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID'"
 
-ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxCheckNS.Mobile/VaxCheckNS.Mobile.Android/Properties/AndroidManifest.xml
 INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxCheckNS.Mobile/VaxCheckNS.Mobile.iOS/Info.plist
-
-if [ -e "$ANDROID_MANIFEST_FILE" ]
-then
-    echo "Updating version name to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID' in AndroidManifest.xml"
-    sed -i '' 's/versionName="[0-9.]*"/versionName="'$MAJOR.$MINOR.$APPCENTER_BUILD_ID'"/' $ANDROID_MANIFEST_FILE
-
-    echo "File content:"
-    cat $ANDROID_MANIFEST_FILE
-fi
-
 
 if [ -e "$INFO_PLIST_FILE" ]
 then
-    echo "Updating version name to '$MAJOR'.'$MINOR'.'$APPCENTER_BUILD_ID' in Info.plist"
+    echo "Updating version name to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID' in Info.plist"
     plutil -replace CFBundleShortVersionString -string $VERSION_NAME $INFO_PLIST_FILE
     plutil -replace CFBundleVersion -string "{$MAJOR}.{$MINOR}.{$APPCENTER_BUILD_ID}" $INFO_PLIST_FILE
 
