5b13e819e83fcb99d1b81cdc5379ecb1ce14672c
Andrew.Kenny <andrewkenny@T4Gs-MacBook-Pro-3.local>
2021-10-21 00:17:27 -0300

    Removed version changes as they cause issues with testflight and app store.

--- VaxCheckNS.Mobile/VaxCheckNS.Mobile.iOS/appcenter-pre-build.sh
+++ VaxCheckNS.Mobile/VaxCheckNS.Mobile.iOS/appcenter-pre-build.sh
@@ -3,27 +3,29 @@
 # For Xamarin Android or iOS, change the version name located in AndroidManifest.xml and Info.plist. 
 # AN IMPORTANT THING: YOU NEED DECLARE VERSION_NAME ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION.
 
-if [ -z "$MAJOR" ]
-then
-    echo "You need define the MAJOR variable in App Center"
-    exit
-fi
+echo "No work to be done."
 
-if [ -z "$MINOR" ]
-then
-    echo "You need define the MINOR variable in App Center"
-    exit
-fi
+# if [ -z "$MAJOR" ]
+# then
+#     echo "You need define the MAJOR variable in App Center"
+#     exit
+# fi
 
-echo "Looking to update version to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID'"
+# if [ -z "$MINOR" ]
+# then
+#     echo "You need define the MINOR variable in App Center"
+#     exit
+# fi
 
-INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxCheckNS.Mobile/VaxCheckNS.Mobile.iOS/Info.plist
+# echo "Looking to update version to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID'"
 
-if [ -e "$INFO_PLIST_FILE" ]
-then
-    echo "Updating version name to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID' in Info.plist"
-    plutil -replace CFBundleShortVersionString -string "$MAJOR.$MINOR.$APPCENTER_BUILD_ID" $INFO_PLIST_FILE
+# INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxCheckNS.Mobile/VaxCheckNS.Mobile.iOS/Info.plist
 
-    echo "File content:"
-    cat $INFO_PLIST_FILE
-fi
+# if [ -e "$INFO_PLIST_FILE" ]
+# then
+#     echo "Updating version name to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID' in Info.plist"
+#     plutil -replace CFBundleShortVersionString -string "$MAJOR.$MINOR.$APPCENTER_BUILD_ID" $INFO_PLIST_FILE
+# 
+#     echo "File content:"
+#     cat $INFO_PLIST_FILE
+# fi
