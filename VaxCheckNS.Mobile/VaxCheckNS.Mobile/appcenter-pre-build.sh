9dc0086dd6d61b52be5188abb7d42e3b7dfc27c5
AjaySivanesu <82045169+AjaySivanesuMNP@users.noreply.github.com>
2021-11-22 10:07:58 -0500

    Update appcenter-pre-build.sh

--- VaxCheckNS.Mobile/VaxCheckNS.Mobile/appcenter-pre-build.sh
+++ VaxCheckNS.Mobile/VaxCheckNS.Mobile/appcenter-pre-build.sh
@@ -43,11 +43,11 @@ APP_SETTINGS_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxCheckNS.Mobile/VaxCheckNS.Mobil
 
 echo $APP_SETTINGS_FILE;
 
-#if [ -e "$APP_SETTINGS_FILE" ]
-#then
-#    echo "Updating AppCenterAndroidKey to $AppCenterAndroidKey in AppSettings.cs"
+if [ -e "$APP_SETTINGS_FILE" ]
+then
+    echo "Updating AppCenterAndroidKey to $AppCenterAndroidKey in AppSettings.cs"
 #    sed -i '' 's#AppCenterAndroidKey = "[-A-Za-z0-9:_./]*"#AppCenterAndroidKey = "'$AppCenterAndroidKey'"#' $APP_SETTINGS_FILE
 #
 #    echo "File content:"
 #    cat $APP_SETTINGS_FILE
-#fi
+fi
