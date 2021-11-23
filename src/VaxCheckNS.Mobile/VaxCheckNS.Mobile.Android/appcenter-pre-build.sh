8a052bca2628308a19bb98cbac6ddb8037ac8d85
AjaySivanesu <82045169+AjaySivanesuMNP@users.noreply.github.com>
2021-11-23 13:09:26 -0500

    Update appcenter-pre-build.sh

--- src/VaxCheckNS.Mobile/VaxCheckNS.Mobile.Android/appcenter-pre-build.sh
+++ src/VaxCheckNS.Mobile/VaxCheckNS.Mobile.Android/appcenter-pre-build.sh
@@ -6,24 +6,26 @@ echo "$PWD"
 
 echo $APPCENTER_SOURCE_DIRECTORY
 
-AppSettingFile=$APPCENTER_SOURCE_DIRECTORY/VaxCheckNS.Mobile/VaxCheckNS.Mobile/Helpers/AppSettings.cs
+AppSettingFile=$APPCENTER_SOURCE_DIRECTORY/src/VaxCheckNS.Mobile/VaxCheckNS.Mobile/Helpers/AppSettings.cs
 
 echo $AppSettingFile
 
 
 if [ -z "$AppCenterAndroidKey" ]
 then
-    echo "You need define the API_URL variable in App Center"
+    echo "You need define the AppSettingFile variable in App Center"
     exit
 fi
 
 if [ -e "$AppSettingFile" ]
 then
     echo "Updating AppCenter Key to $AppCenterAndroidKey in AppSetting.cs"
-    sed -i '' 's#AppCenterAndroidKey = "[-A-Za-z0-9:_./]*"#AppCenterAndroidKey = "'$AppCenterAndroidKey'"#' $$AppSettingFile
+    sed -i '' 's#AppCenterAndroidKey = "[-A-Za-z0-9:_./]*"#AppCenterAndroidKey = "'$AppCenterAndroidKey'"#' $AppSettingFile
 
     echo "File content:"
-    cat $$AppSettingFile
+    cat $AppSettingFile
+else
+    echo "Did not find AppSettings file."
 fi
+
 echo "END-SCRIPT"
-printf "END-SCRIPT"
