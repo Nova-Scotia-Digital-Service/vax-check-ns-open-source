763df5725a9c621648373553075c2d8ce36073b1
AjaySivanesu <82045169+AjaySivanesuMNP@users.noreply.github.com>
2021-11-23 12:29:28 -0500

    Update appcenter-pre-build.sh

--- src/VaxCheckNS.Mobile/VaxCheckNS.Mobile.Android/appcenter-pre-build.sh
+++ src/VaxCheckNS.Mobile/VaxCheckNS.Mobile.Android/appcenter-pre-build.sh
@@ -4,30 +4,6 @@ echo "$pwd"
 
 echo $APPCENTER_SOURCE_DIRECTORY
 
-echo "Looking to update version to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID'"
+AppSettingFile=$APPCENTER_SOURCE_DIRECTORY/VaxCheckNS.Mobile/VaxCheckNS.Mobile/Helpers/AppSettings.cs
 
-if [ -z "$MAJOR" ]
-then
-    echo "You need define the MAJOR variable in App Center"
-    exit
-fi
-
-if [ -z "$MINOR" ]
-then
-    echo "You need define the MINOR variable in App Center"
-    exit
-fi
-
-
-
-ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxPassPEI.Mobile/VaxPassPEI.Mobile.Android/Properties/AndroidManifest.xml
-
-if [ -e "$ANDROID_MANIFEST_FILE" ]
-then
-    echo "Updating version name to '$MAJOR.$MINOR.$APPCENTER_BUILD_ID' in AndroidManifest.xml"
-    sed -i '' 's/versionName="[0-9.]*"/versionName="'$MAJOR.$MINOR.$APPCENTER_BUILD_ID'"/' $ANDROID_MANIFEST_FILE
-    sed -i '' 's/versionCode="[0-9.]*"/versionCode="'$APPCENTER_BUILD_ID'"/' $ANDROID_MANIFEST_FILE
-
-    echo "File content:"
-    cat $ANDROID_MANIFEST_FILE
-fi
+echo $AppSettingFile
