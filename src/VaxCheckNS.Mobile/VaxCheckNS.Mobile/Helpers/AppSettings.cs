a3269f4ddf288e6062dbe7fc8451f92027c820c8
AjaySivanesu <82045169+AjaySivanesuMNP@users.noreply.github.com>
2021-11-24 11:19:16 -0500

    remove hardcoded keys

--- src/VaxCheckNS.Mobile/VaxCheckNS.Mobile/Helpers/AppSettings.cs
+++ src/VaxCheckNS.Mobile/VaxCheckNS.Mobile/Helpers/AppSettings.cs
@@ -6,14 +6,10 @@ namespace VaxCheckNS.Mobile.Helpers
 {
     public static class AppSettings
     {
-        public static string AppPublicKey = "";
-        public static string AppPrivateKey = "A7848922-C10A-4D6A-9D82-4987F638F718";
+        public static string AppPrivateKey = "A7848922-C10A-4D6A-9D82-4987F638F718"; //Used only internally for thread lock
 
         public static string AppCenterAndroidKey = "FAKE-ID";
         public static string AppCenteriOSKey = "FAKE-ID";
-
-        //public static string AppCenterAndroidKey = "c8da9da7-50dd-42a8-b7f4-312370215d5a";
-        //public static string AppCenteriOSKey = "b680b5fc-214c-49a1-9bce-5decfe8b445a";
 
         public static TimeSpan JWKSUpdateThreshold => new TimeSpan(7, 0, 0, 0, 0); 
     }
