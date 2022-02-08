adda89086adf85dab49385a4e6ea1cd7390b57fc
Sivanesu, Ajay <ajay.sivanesu@novascotia.ca>
2022-02-08 13:18:58 -0400

    Removed App Center

--- src/VaxCheckNS.Mobile/VaxCheckNS.Mobile/Services/ErrorManagementService.cs
+++ src/VaxCheckNS.Mobile/VaxCheckNS.Mobile/Services/ErrorManagementService.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,62 +7,62 @@ using Xamarin.Essentials;
 
 namespace VaxCheckNS.Mobile.Services
 {
-	public interface IErrorManagementService
-	{
-		void HandleError(Exception ex);
-		void BadScan(ProofOfVaccinationData data, string message);
-		void ReportEvent(string eventName, string message);
-	}
+    public interface IErrorManagementService
+    {
+        void HandleError(Exception ex);
+        void BadScan(ProofOfVaccinationData data, string message);
+        void ReportEvent(string eventName, string message);
+    }
 
-	public class ErrorManagementService : IErrorManagementService
-	{
+    public class ErrorManagementService : IErrorManagementService
+    {
 
-		public ErrorManagementService()
-		{
+        public ErrorManagementService()
+        {
 
-		}
+        }
 
-		public void HandleError(Exception ex)
-		{
+        public void HandleError(Exception ex)
+        {
 #if RELEASE
             Report(ex);
 #endif
-		}
+        }
 
-		private void Report(Exception ex)
-		{
-			Crashes.TrackError(ex);
-		}
+        private void Report(Exception ex)
+        {
+            //Crashes.TrackError(ex);
+        }
 
-		private void InternalReport(Exception ex)
-		{
+        private void InternalReport(Exception ex)
+        {
 
-		}
+        }
 
-		public void BadScan(ProofOfVaccinationData data, string message)
-		{
+        public void BadScan(ProofOfVaccinationData data, string message)
+        {
 
-			Analytics.TrackEvent("Invalid Scan",
-				  new Dictionary<string, string>
-				  {
-					{ "Time", DateTime.UtcNow.ToString() },
-					{ "Issuer", data.Issuer},
-					{ "ExceptionMessage", message},
-					{ "Version", VersionTracking.CurrentVersion},
-					{ "Platform", DeviceInfo.Platform.ToString()}
-				  });
-		}
+            //Analytics.TrackEvent("Invalid Scan",
+            //	  new Dictionary<string, string>
+            //	  {
+            //		{ "Time", DateTime.UtcNow.ToString() },
+            //		{ "Issuer", data.Issuer},
+            //		{ "ExceptionMessage", message},
+            //		{ "Version", VersionTracking.CurrentVersion},
+            //		{ "Platform", DeviceInfo.Platform.ToString()}
+            //	  });
+        }
 
-		public void ReportEvent(string eventName, string message)
-		{
-			Analytics.TrackEvent(eventName,
-				  new Dictionary<string, string>
-				  {
-					{ "Time", DateTime.UtcNow.ToString() },
-					{ "Message", message},
-					{ "Version", VersionTracking.CurrentVersion},
-					{ "Platform", DeviceInfo.Platform.ToString()}
-				  });
-		}
-	}
+        public void ReportEvent(string eventName, string message)
+        {
+            //Analytics.TrackEvent(eventName,
+            //	  new Dictionary<string, string>
+            //	  {
+            //		{ "Time", DateTime.UtcNow.ToString() },
+            //		{ "Message", message},
+            //		{ "Version", VersionTracking.CurrentVersion},
+            //		{ "Platform", DeviceInfo.Platform.ToString()}
+            //	  });
+        }
+    }
 }
